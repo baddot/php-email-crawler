3c69f47c536ed57c5a0360eebdf05e6f8df55844
icysheep <cowthinker@gmail.com>
2013-10-26 00:08:43 +0200

    see changelog

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,1 +1,7 @@
 v1.0 Initial Release
+
+v1.01
+- Removed unnecessary code
+- Changed style of output (using print_r now)
+- Improved url regex pattern (experimental)
+- Improved email regex pattern (experimental)
--- crawl.php
+++ crawl.php
@@ -24,13 +24,13 @@
   */
  
     echo "==================================================\r\n";
-    echo " Welcome to Crawl 1.0 (Initial Release)\r\n";
+    echo " Welcome to Crawl 1.01 \r\n";
     echo "==================================================\r\n";
     echo " Make sure cURL is activated for best perfomance  \r\n";
     echo "==================================================\r\n";
     if(!isset($argv[2])) {
-        echo " Usage: ".$argv[0]." HOST r_Level\r\n";
-        echo " Example: ".$argv[0]." http://www.theverge.com/ 3\r\n";
+        echo " Usage: php ".$argv[0]." HOST r_Level\r\n";
+        echo " Example: php ".$argv[0]." http://www.theverge.com/ 3\r\n";
         echo "==================================================\r\n";
         exit();
     }
@@ -38,7 +38,7 @@
         echo " Working... time depends on recursion level\r\n";
         echo "==================================================\r\n";
         $start = new Crawl($argv[1], 0, $argv[2], array());
-        var_dump($start->start());
+        $start->printResult($start->start());
         exit();
     }
 	
@@ -65,33 +65,25 @@ class Crawl{
     }
 	
     /**
-     * Get the content of the current page ($this->hp) with file_get_contents()
+     * Get the content of the current page ($this->hp)
      * @return string
      */
     public function getContent() {
         if (!function_exists('curl_init')){
-            return htmlentities(@file_get_contents($this->hp, false, $this->getContext()), ENT_QUOTES, 'utf-8');
+            $content=file_get_contents($this->hp, false, $this->getContext());
+        } 
+		else {
+            $ch = curl_init();
+            curl_setopt($ch, CURLOPT_URL, $this->hp);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            $content = curl_exec($ch);
+            curl_close($ch);
         }
-        else {
-            return htmlentities($this->curl_getContent(), ENT_QUOTES, 'utf-8');
-        }
+		return $content;
     }
- 
+  
     /**
-     * Get the content of the current page ($this->hp) with cURL
-     * @return string
-     */
-    private function curl_getContent() {
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $this->hp);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        $output = curl_exec($ch);
-        curl_close($ch);
-        return $output;
-    }
- 
-    /**
-     * Get the context to open the cURL socket
+     * Get the context to open the FGC socket
      * @return stream_context_resource
      */
     private function getContext() {
@@ -105,14 +97,17 @@ class Crawl{
     }
 	
     /**
-     * Use the web content to create an email array
+     * Use the content to create an email array
      * Make sure we don't save the same email address multiple times
      * @return array
      */
     public  function getEmailArray() {
         $email_pattern_normal="(([-_.\w]+@[a-zA-Z0-9_]+?\.[a-zA-Z0-9]{2,6}))";
-        preg_match_all($email_pattern_normal, $this->content, $result_email, PREG_PATTERN_ORDER);
-        $unique_emails=$this->array_unique_deep($result_email);
+		$email_pattern_exp1="(\w[-._\w]*\w@\w[-._\w]*\w\.\w{2,3})";
+        preg_match_all($email_pattern_normal, $this->content, $result_email_normal, PREG_PATTERN_ORDER);
+		preg_match_all($email_pattern_exp1, $this->content, $result_email_exp1, PREG_PATTERN_ORDER);
+        $email_array=array_merge($result_email_normal, $result_email_exp1);
+		$unique_emails=$this->array_unique_deep($email_array);
         return $unique_emails;
     }
  
@@ -123,8 +118,12 @@ class Crawl{
     private function array_unique_deep($array) {
         $values=array();
         foreach ($array as $part) {
-            if (is_array($part)) $values=array_merge($values,$this->array_unique_deep($part));
-            else $values[]=$part;
+            if (is_array($part)) {
+				$values=array_merge($values,$this->array_unique_deep($part));
+			}
+            else { 
+				$values[]=$part;
+			}
         }
         return array_unique($values);
     }
@@ -135,8 +134,9 @@ class Crawl{
      * @return array
      */
     public function getURLArray() {
-        $url_pattern= '((http:\/\/|https:\/\/|www\.)[a-zA-Z0-9\-\.]{2,}\.([a-zA-Z.]{2,5}))i';
+	    $url_pattern='((\:href=\"|(http(s?))\://){1}\S+)';
         preg_match_all($url_pattern, $this->content, $result_url, PREG_PATTERN_ORDER);
+	    array_walk($result_url[0], function(&$item) { $item = substr($item, 0, strpos($item, '"')); });
         $unique_urls=$this->array_unique_deep($result_url[0]);
         $unique_urls=array_unique($this->setURLPrefix($unique_urls));
         return $unique_urls;
@@ -158,24 +158,32 @@ class Crawl{
         return $prefix_array;
     }
 	
-     /**
-      * Start function with recursion
-      * Creates new instances depending on recursion depth
-      * Merges the obtained email addresses and returns them
-      * @return mails
-      */
-     public  function start() {
-        if($this->rlevel<$this->rmax) {
-            $this->content = $this->getContent();
-            $this->urls = $this->getURLArray();
-            $mails = $this->getEmailArray();
-            foreach($this->urls as $url) {
-               $temp = new Crawl($url, $this->rlevel+1, $this->rmax, $this->mails);
-               $this->mails = array_unique(array_merge($temp->start(), $mails));
-            }
-        }
-        return $this->mails;
+	/**
+	 * Temporarily function to print the result
+	 */
+	public function printResult($data) {
+		print_r($data);
+	}
+	
+    /**
+     * Start function with recursion
+     * Creates new instances depending on recursion depth
+     * Merges the obtained email addresses and returns them
+     * @return mails
+     */
+    public function start() {
+       if($this->rlevel<$this->rmax) {
+           $this->content = $this->getContent();
+           $this->urls = $this->getURLArray();
+           $mails = $this->getEmailArray();
+           foreach($this->urls as $url) {
+              $temp = new Crawl($url, $this->rlevel+1, $this->rmax, $this->mails);
+              $this->mails = array_unique(array_merge($temp->start(), $mails));
+           }
+       }
+       return $this->mails;
     }
 }
 	
 ?>
+
